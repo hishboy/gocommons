d1075d64a024613940c3118e1e1f0f93c971764c
Hicham Bouabdallah <hbouabdallah@pandora.com>
2013-10-07 15:38:12 -0700

    added arraylist

--- lang/queue.go
+++ lang/queue.go
@@ -1,4 +1,3 @@
-
 package lang
 
 
--- main.go
+++ main.go
@@ -27,4 +27,41 @@ func main() {
 	fmt.Println("Total items before pop: ", stack.Count())
 	fmt.Println(stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop())
 	fmt.Println("Total items after pop: ", stack.Count())
+	
+	
+	array := lang.NewArrayList()
+	array.Add("hello")
+	array.Add("world")
+	array.Add("Hicham")
+	fmt.Println("total items in array:", array.Count())
+	fmt.Println("array:", array)
+	fmt.Println("array[1]:", array.Get(1))
+	fmt.Println("array.indexOf(world):", array.IndexOf("world"))
+	fmt.Println("array.Contains(world):", array.Contains("world"), array.Contains("random"))
+	
+	for i := 0; i < array.Count(); i++ {
+		fmt.Println("item(", i, "):", array.Get(i))
+	}
+	
+	fmt.Println("array.Remove(world):", array.Remove("world"), array)
+	fmt.Println("array.Remove(Hicham):", array.Remove("Hicham"), array)
+	fmt.Println("array.IsEmpty():", array.IsEmpty(), array.Remove("hello"), array.IsEmpty(), array.Count())
+	
+	for i := 0; i < 30; i++ {
+		array.Add(i)
+	}
+	
+	fmt.Println("array.indexOf(5):", array.IndexOf(5), array.Count())
+	array.Clear()
+	array.Add("one", "two", "three")
+	
+	array2 := lang.NewArrayList()
+	array2.Add("four", "five", "six")
+	
+	array.AddFromArrayList(array2)
+	
+	fmt.Println("array:", array, array.Count())
+	fmt.Println("array.First/Last:", array.First(), array.Last())
+	fmt.Println("array.ToSlice:", array.ToSlice())
+	
 }
