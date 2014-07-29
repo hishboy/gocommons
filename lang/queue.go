faf791c0a6915ed7dc2642f25f65a07204640585
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2014-07-28 21:42:55 -0500

    added test to see if nil was treated correctly as an elements and more compliated tests

--- lang/queue.go
+++ lang/queue.go
@@ -28,7 +28,6 @@
 
 package lang
 
-
 import "sync"
 
 type queuenode struct {
@@ -36,32 +35,36 @@ type queuenode struct {
 	next *queuenode
 }
 
-
+//	FIFO (first in first out) data stucture.
 type Queue struct {
-	head *queuenode
-	tail *queuenode
+	head  *queuenode
+	tail  *queuenode
 	count int
-	lock *sync.Mutex
+	lock  *sync.Mutex
 }
 
+//	Creates a new pointer to a new queue.
 func NewQueue() *Queue {
 	q := &Queue{}
 	q.lock = &sync.Mutex{}
 	return q
 }
 
+//	Returns the number of elements in the queue (i.e. size/length)
 func (q *Queue) Len() int {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 	return q.count
 }
 
+//	Pushes/inserts a value at the end/tail of the queue.
+//	Note: this function does mutate the queue.
 func (q *Queue) Push(item interface{}) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
-	
-	n := &queuenode { data: item }
-	
+
+	n := &queuenode{data: item}
+
 	if q.tail == nil {
 		q.tail = n
 		q.head = n
@@ -71,34 +74,40 @@ func (q *Queue) Push(item interface{}) {
 	}
 	q.count++
 }
- 
+
+//	Returns the value at the front of the queue.
+//	i.e. the oldest value in the queue.
+//	Note: this function does mutate the queue.
 func (q *Queue) Poll() interface{} {
 	q.lock.Lock()
 	defer q.lock.Unlock()
-	
+
 	if q.head == nil {
 		return nil
 	}
-	
+
 	n := q.head
 	q.head = n.next
-	
+
 	if q.head == nil {
 		q.tail = nil
 	}
 	q.count--
-	
+
 	return n.data
 }
 
+//	Returns a read value at the front of the queue.
+//	i.e. the oldest value in the queue.
+//	Note: this function does NOT mutate the queue.
 func (q *Queue) Peek() interface{} {
 	q.lock.Lock()
 	defer q.lock.Unlock()
-	
+
 	n := q.head
 	if n == nil || n.data == nil {
 		return nil
 	}
-	
+
 	return n.data
 }
